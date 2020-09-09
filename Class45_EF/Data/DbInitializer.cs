dce744a39a53fde7eada786ac0ed581bca43a21c
steve <jedi1014@comcast.net>
2020-09-09 18:29:31 -0400

    initial commit

---
+++ Class45_EF/Data/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Class45_EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class45_EF.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(LibraryContext context)
+        {
+            context.Database.EnsureCreated();
+            if (!context.Books.Any())
+            {
+                context.Books.Add(new Book() { Title = "Harry Potter and the Sorcerer's Stone", Genre = "Fantasy", ReleaseDate = DateTime.Parse("6/14/1997") });
+                context.Books.Add(new Book() { Title = "A Tale of Two Cities", Genre = "Fiction", ReleaseDate = DateTime.Parse("12/25/1859") });
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Class45_EF/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Class45_EF.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+    }
+}
